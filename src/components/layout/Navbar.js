792637f7f4f86233ac624d234df766f57c82c4b6
HeartsOnSunday <leahgdg@hotmail.com>
2018-09-17 20:35:05 -0400

    created Navbar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <link to="/" className="brand-logo">
+          Stupid Mario Plan
+        </link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
